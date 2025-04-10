4de77a198ec63b47900a21ea9e7c3e1dec7f51b6
kimchanho97 <nh0903@pusan.ac.kr>
2025-04-11 05:21:24 +0900

    feat: 카프카 메시지 프로듀서 구현

---
+++ src/main/java/com/sunsuwedding/chat/kafka/producer/ChatMessageProducer.java
@@ -0,0 +1,29 @@
+package com.sunsuwedding.chat.kafka.producer;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sunsuwedding.chat.dto.ChatMessage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ChatMessageProducer {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final ObjectMapper objectMapper;
+
+    public void send(ChatMessage message) {
+        try {
+            String payload = objectMapper.writeValueAsString(message);
+
+            log.info("[Kafka Producer] 메시지 전송: {}", payload);
+            kafkaTemplate.send("chat-message", payload);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Kafka 메시지 직렬화 실패", e);
+        }
+    }
+}
