d384f5d9e03609490b97f0b9296483c7869a77eb
kimchanho97 <nh0903@pusan.ac.kr>
2025-04-11 20:13:17 +0900

    feat: MongoDB 리포지토리 정의

--- src/main/java/com/sunsuwedding/chat/repository/ChatMessageMongoRepository.java
+++ src/main/java/com/sunsuwedding/chat/repository/ChatMessageMongoRepository.java
@@ -2,13 +2,12 @@ package com.sunsuwedding.chat.repository;
 
 import com.sunsuwedding.chat.domain.ChatMessageDocument;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface ChatMessageMongoRepository extends MongoRepository<ChatMessageDocument, String> {
-    
-    List<ChatMessageDocument> findByChatRoomIdOrderByCreatedAtAsc(Long chatRoomId, Pageable pageable);
+
+    Slice<ChatMessageDocument> findByChatRoomIdOrderByCreatedAtDesc(Long chatRoomId, Pageable pageable);
 }
