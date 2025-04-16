d014b16a0d4f39a1018e9a926dfc5959135b6553
kimchanho97 <nh0903@pusan.ac.kr>
2025-04-17 04:30:14 +0900

    refactor: Client 내부 http 통신 예외 처리

--- src/main/java/com/sunsuwedding/chat/client/ChatRoomApiClient.java
+++ src/main/java/com/sunsuwedding/chat/client/ChatRoomApiClient.java
@@ -1,11 +1,14 @@
 package com.sunsuwedding.chat.client;
 
+import com.sunsuwedding.chat.common.exception.ChatErrorCode;
+import com.sunsuwedding.chat.common.exception.CustomException;
 import com.sunsuwedding.chat.dto.room.ChatRoomCreateRequest;
 import com.sunsuwedding.chat.dto.room.ChatRoomCreateResponse;
 import com.sunsuwedding.chat.dto.room.ChatRoomValidationRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 @Component
@@ -14,20 +17,28 @@ public class ChatRoomApiClient {
 
     private final RestTemplate restTemplate;
 
+    private final static String CHAT_ROOM_PATH = "/internal/chat-rooms";
+
     @Value("${backend.api.base-url}")
     private String baseUrl;
 
-    @Value("${backend.api.chat-room-path}")
-    private String chatRoomPath;
 
     public ChatRoomCreateResponse createOrFindChatRoom(ChatRoomCreateRequest request) {
-        String url = baseUrl + chatRoomPath;
-        return restTemplate.postForObject(url, request, ChatRoomCreateResponse.class);
+        String url = baseUrl + CHAT_ROOM_PATH;
+        try {
+            return restTemplate.postForObject(url, request, ChatRoomCreateResponse.class);
+        } catch (RestClientException e) {
+            throw new CustomException(ChatErrorCode.CHAT_ROOM_API_FAILED);
+        }
     }
 
     public boolean validateChatRoom(ChatRoomValidationRequest request) {
-        String url = baseUrl + chatRoomPath + "/validate";
-        return restTemplate.postForObject(url, request, Boolean.class);
+        String url = baseUrl + CHAT_ROOM_PATH + "/validate";
+        try {
+            return Boolean.TRUE.equals(restTemplate.postForObject(url, request, Boolean.class));
+        } catch (RestClientException e) {
+            throw new CustomException(ChatErrorCode.CHAT_ROOM_VALIDATION_FAILED);
+        }
     }
 
 }
