b1971bc529fe1f265c05b416a4bbd6053d8d28f5
kimchanho97 <nh0903@pusan.ac.kr>
2025-04-17 21:49:37 +0900

    refactor: 카프카 토픽 명시적으로 정의

---
+++ src/main/java/com/sunsuwedding/chat/config/KafkaTopicConfig.java
@@ -0,0 +1,26 @@
+package com.sunsuwedding.chat.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic chatMessageTopic() {
+        return TopicBuilder.name("chat-message")
+                .partitions(1)
+                .replicas(1)
+                .build();
+    }
+
+    @Bean
+    public NewTopic presenceStatusTopic() {
+        return TopicBuilder.name("presence-status")
+                .partitions(1)
+                .replicas(1)
+                .build();
+    }
+}
