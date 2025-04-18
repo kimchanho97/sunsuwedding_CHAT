6f6ea0caa641766cf55a394d13e1d1dcf58c8b64
kimchanho97 <nh0903@pusan.ac.kr>
2025-04-18 23:06:20 +0900

    chore: 클래스명 수정

---
+++ src/main/java/com/sunsuwedding/chat/controller/api/ChatRoomController.java
@@ -0,0 +1,33 @@
+package com.sunsuwedding.chat.controller.api;
+
+import com.sunsuwedding.chat.common.response.ApiResponse;
+import com.sunsuwedding.chat.dto.room.ChatRoomCreateRequest;
+import com.sunsuwedding.chat.dto.room.ChatRoomCreateResponse;
+import com.sunsuwedding.chat.dto.room.ChatRoomValidationRequest;
+import com.sunsuwedding.chat.dto.room.ChatRoomValidationResponse;
+import com.sunsuwedding.chat.service.ChatRoomService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/chat-rooms")
+public class ChatRoomController {
+
+    private final ChatRoomService chatRoomService;
+
+    @PostMapping
+    public ApiResponse<ChatRoomCreateResponse> createChatRoom(@RequestBody @Valid ChatRoomCreateRequest request) {
+        return ApiResponse.success(chatRoomService.createChatRoom(request));
+    }
+
+    @PostMapping("/validate")
+    public ApiResponse<ChatRoomValidationResponse> validate(@RequestBody @Valid ChatRoomValidationRequest request) {
+        boolean isValid = chatRoomService.validateChatRoom(request);
+        return ApiResponse.success(new ChatRoomValidationResponse(isValid));
+    }
+}
