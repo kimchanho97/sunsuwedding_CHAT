62e5c599735482bac046a5569bf48ef4c595a973
kimchanho97 <nh0903@pusan.ac.kr>
2025-04-25 03:13:16 +0900

    fix: 직렬화 시간 추적

--- src/main/java/com/sunsuwedding/chat/model/ChatRoomMeta.java
+++ src/main/java/com/sunsuwedding/chat/model/ChatRoomMeta.java
@@ -1,9 +1,13 @@
 package com.sunsuwedding.chat.model;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
 import java.time.LocalDateTime;
 
 public record ChatRoomMeta(
         String lastMessage,
+
+        @JsonSerialize(using = LoggingLocalDateTimeSerializer.class)
         LocalDateTime lastMessageAt,
         Long lastMessageSeqId
 ) {
