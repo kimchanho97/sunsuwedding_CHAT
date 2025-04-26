29c4d2ae249660d1a5f73b0337c9b9588f8d659c
kimchanho97 <nh0903@pusan.ac.kr>
2025-04-27 03:10:58 +0900

    feat: ChatMessageDocument 복합 인덱스(unique) 설정

--- src/main/java/com/sunsuwedding/chat/model/ChatMessageDocument.java
+++ src/main/java/com/sunsuwedding/chat/model/ChatMessageDocument.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
@@ -14,6 +15,10 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Document(collection = "chat_messages")
+@CompoundIndex(
+        name = "chatRoomCode_messageSeqId_idx",
+        def = "{'chatRoomCode' : 1, 'messageSeqId' : 1}",
+        unique = true) // 복합 인덱스 설정 -> 쿼리 성능 향상 + unique 제약 조건 설정
 public class ChatMessageDocument {
 
     @Id
