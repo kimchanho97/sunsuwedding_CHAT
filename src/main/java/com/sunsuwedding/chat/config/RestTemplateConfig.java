be513443deb1c95b45309b618ecd13326efc987e
kimchanho97 <nh0903@pusan.ac.kr>
2025-04-28 07:30:26 +0900

    feat: 실시간 통신을 위한 RestTemplate 정의

--- src/main/java/com/sunsuwedding/chat/config/RestTemplateConfig.java
+++ src/main/java/com/sunsuwedding/chat/config/RestTemplateConfig.java
@@ -1,14 +1,31 @@
 package com.sunsuwedding.chat.config;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestTemplateConfig {
 
     @Bean
+    @Primary
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(2000);    // 2초 연결 타임아웃
+        factory.setReadTimeout(5000);       // 5초 읽기 타임아웃
+        return new RestTemplate(factory);
     }
+
+    @Bean
+    @Qualifier("realtimeRestTemplate")
+    public RestTemplate realtimeRestTemplate() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(1000);    // 1초 연결 타임아웃
+        factory.setReadTimeout(2000);       // 2초 읽기 타임아웃
+        return new RestTemplate(factory);
+    }
+
 }
